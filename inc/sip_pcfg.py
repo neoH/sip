612741773a425b03795a12592dbc6d032ba540a4
neo <joachim_huang@icloud.com>
2018-11-15 20:38:39 +0800

    sync on macpro

---
+++ inc/sip_pcfg.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+"""
+this is a include module for sip tool. Which will store all program related information.
+"""
+
+## import options module
+import options;
+
+class sip_pcfg:
+
+	## __opt__
+	## internal class member to store the handle of options module,
+	## which means it is an instance of options class.
+
+	def __init__(self,argvs):
+		## instantiate the options class to a class defined var: __opt__;
+
+		## call set_support about all supported options for sip tool
+
+		## call __opt__.load
+
+		##
+
+	## end def }
+
+## end class }
